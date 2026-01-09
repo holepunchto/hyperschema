17ecd53a2140c2de5ca715228a43705b1e601dcc
Sean Zellmer <sean@lejeunerenard.com>
2026-01-09 12:23:03 -0500

    Fix typo in inlined array checked

--- builder.cjs
+++ builder.cjs
@@ -405,7 +405,7 @@ class Struct extends ResolvedType {
         if (!field.type.compact) throw new Error(`Struct ${this.fqn}: inline requires compact`)
         if (fieldDescription.required)
           throw new Error(`Struct ${this.fqn}: inline cannot be required`)
-        if (fieldDescription.inline) {
+        if (fieldDescription.array) {
           throw new Error(`Struct ${this.fqn}: Arrays cannot be inlined`)
         }
       }
