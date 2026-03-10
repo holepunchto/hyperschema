bb9e4bd21a3da51c5a65634d1115bcf2193b0c44
Balakrishna Avulapati <ba@bavulapati.com>
2026-03-10 18:03:09 +0530

    Add iOS platform to the package.swift
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- test/swift-workspace/Package.swift
+++ test/swift-workspace/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "HyperschemaTest",
-  platforms: [.macOS(.v11)],
+  platforms: [.macOS(.v11), .iOS(.v14)],
   dependencies: [
     .package(url: "https://github.com/holepunchto/compact-encoding-swift", branch: "main")
   ],
